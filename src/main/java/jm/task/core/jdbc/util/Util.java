212ea5d33543731de05b177d389b98cc9d181132
MaximBachok <staysoulbyme@gmail.com>
2021-12-22 20:39:21 +0400

    two

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,16 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/users";
+        String username = "root";
+        String password = "602507qweASD";
+        try (Connection connection = DriverManager.getConnection(url, username, password)){
+
+        }
+    }
 }
