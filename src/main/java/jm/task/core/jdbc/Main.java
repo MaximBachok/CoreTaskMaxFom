a976963c4a0feee8f029bc37097fa997170e8817
MaximBachok <staysoulbyme@gmail.com>
2021-12-25 16:12:06 +0400

    2.0

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -4,8 +4,6 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
-import java.sql.SQLOutput;
-
 public class Main {
     public static void main(String[] args) {
         UserService userService = new UserServiceImpl();
